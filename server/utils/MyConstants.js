dc3466714ec006aa67daf14eb18dfa6c3bc81698
Liam0m7 <buinhatcuong.bmt@gmail.com>
2023-07-20 09:04:56 +0700

    add 3 file

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+  const MyConstants = {
+    DB_SERVER: 'shoppingonline.zlyfouo.mongodb.net',
+    DB_USER: 'nhatcuong',
+    DB_PASS: 'Cuong123',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'nhatcuong.0711.20@gmail.com', // Microsoft mail service 
+    EMAIL_PASS: 'fxgqlwfqndmytyot', // 
+    JWT_SECRET: 'shoppingonline',       // 
+    JWT_EXPIRES: '2592000000', // in milliseconds = 30x24x60x60
+  };
+  
+  module.exports = MyConstants;
